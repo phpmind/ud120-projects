e211d88596ebcaf3d362756878827d0d00922ad7
Daniel Hunter <officialdanielhunter@gmail.com>
2017-11-16 13:17:27 -0800

    Finished feature selection, also added a bit to decision tree lesson

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
 
 import pickle
-import numpy
-numpy.random.seed(42)
+import numpy as np
+np.random.seed(42)
 
 
 ### The words (features) and authors (labels), already largely processed.
 ### These files should have been created from the previous (Lesson 10)
 ### mini-project.
-words_file = "../text_learning/your_word_data.pkl" 
+words_file = "../text_learning/your_word_data.pkl"
 authors_file = "../text_learning/your_email_authors.pkl"
 word_data = pickle.load( open(words_file, "r"))
 authors = pickle.load( open(authors_file, "r") )
@@ -31,13 +31,20 @@ features_test  = vectorizer.transform(features_test).toarray()
 
 ### a classic way to overfit is to use a small number
 ### of data points and a large number of features;
-### train on only 150 events to put ourselves in this regime
+### train on only 150 events to put ourselves in this position
 features_train = features_train[:150].toarray()
 labels_train   = labels_train[:150]
 
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
 
+# print type(clf.feature_importances_)
+for i, val in enumerate(clf.feature_importances_):
+    if val > .2:
+        print vectorizer.get_feature_names()[i], val
 
-### your code goes here
-
-
+from sklearn.metrics import accuracy_score
+print accuracy_score(pred, labels_test)
 
--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -39,37 +39,50 @@ temp_counter = 0
 
 for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
+        # print path
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
+        # temp_counter += 1
         if temp_counter < 200:
             path = os.path.join('..', path[:-1])
-            print path
+            # print path
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
+            words = parseOutText(email)
+
 
             ### use str.replace() to remove any instances of the words
-            ### ["sara", "shackleton", "chris", "germani"]
+            remove = ['sara', 'shackleton', 'chris', 'germani', 'sshacklensf',
+            'cgermannsf']
+            for word in remove:
+                words = words.replace(word, '')
 
             ### append the text to word_data
+            word_data.append(words)
 
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            if name == 'sara':
+                from_data.append(0)
+            else:
+                from_data.append(1)
 
             email.close()
 
-print "emails processed"
 from_sara.close()
 from_chris.close()
 
 pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
-
-
-
+# print word_data[152]
 
 ### in Part 4, do TfIdf vectorization here
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-
+tfidf = TfidfVectorizer(stop_words = 'english')
+tfidf.fit(word_data)
+tfidf.transform(word_data)
+# print tfidf.get_stop_words()
+# print len(tfidf.get_feature_names())
+# print tfidf.get_feature_names()[34597]
